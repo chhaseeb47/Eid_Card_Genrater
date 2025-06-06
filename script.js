559677fb64603f156df1c3213c0fd5828dd7b571
CH HASEEB <mhmodsyt517@gmail.com>
2025-06-06 14:59:11 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+javascript
+const wishes = [
+  "May this Eid bring peace in your life 🕊️\nBlessings in your home 🏡\nLove in your heart ❤️\nBarakah in your earnings 💵\nAnd joy in every step! 🌟",
+  "Dear family, Eid Mubarak! 🐏\nTogetherness is our biggest gift 🎁\nMay Allah keep us united 🤲\nGrant us happiness & health 🌸\nLet’s enjoy this Eid to the fullest! 😍",
+  "To my respected elders and sweet cousins 👨‍👩‍👧‍👦\nWishing u an Eid full of joy 🌙\nMay your prayers be accepted 🕋\nYour wishes fulfilled 💫\nAnd your hearts at peace 🤍",
+  "Eid-ul-Adha Mubarak dear uncle & aunt! 🐐\nYour love means the world to us 💕\nMay Allah bless your days ahead 🌟\nFill your lives with joy 😄\nAnd your plates with BBQ 🍖",
